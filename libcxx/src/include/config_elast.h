83b90aad72aba89a5114d7fd01289299dc2fa868
Daniel Bittman <danielbittman1@gmail.com>
2025-07-22 15:45:35 -0700

    Fixing libc bugs.

--- libcxx/src/include/config_elast.h
+++ libcxx/src/include/config_elast.h
@@ -33,7 +33,7 @@
 // No _LIBCPP_ELAST needed on WASI
 #elif defined(__EMSCRIPTEN__)
 // No _LIBCPP_ELAST needed on Emscripten
-#elif defined(__linux__) || _LIBCPP_HAS_MUSL_LIBC
+#elif defined(__linux__) || _LIBCPP_HAS_MUSL_LIBC || defined(__Twizzler__)
 #  define _LIBCPP_ELAST 4095
 #elif defined(__APPLE__)
 // No _LIBCPP_ELAST needed on Apple
